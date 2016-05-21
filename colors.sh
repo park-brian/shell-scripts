3e7c71b496d874e81b41a4d80eace81b4f515f47
Brian Park <park-brian@users.noreply.github.com>
2016-05-20 23:39:55 -0400

    Create colors.sh

---
+++ colors.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Adds console colors
+
+alias ls='ls --color=auto'
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
+BLACK="\e[30m"
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+MAGENTA="\e[35m"
+CYAN="\e[36m"
+LIGHTGRAY="\e[37m"
+DARKGRAY="\e[90m"
+LIGHTRED="\e[91m"
+LIGHTGREEN="\e[92m"
+LIGHTYELLOW="\e[93m"
+LIGHTBLUE="\e[94m"
+LIGHTMAGENTA="\e[95m"
+LIGHTCYAN="\e[96m"
+WHITE="\e[97m"
+
+RESET="\[$(tput sgr0)\]"
+
+export PS1="${GREEN}\u${LIGHTRED}@${CYAN}\h ${LIGHTBLUE}\w${RESET} $ "
