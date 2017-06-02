d222a21aaa06da26656c580f6eb44d96d6cc9c8a
park-brian <re.public@outlook.com>
2017-06-02 17:41:59 -0400

    update start scripts

--- misc/wamp/start_bash.sh
+++ misc/wamp/start_bash.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export PATH=$PATH:$PWD/php
+export PATH=$PATH:$PWD/php:$PWD/bin:$PWD/mysql/bin
 export COMPOSER_PROCESS_TIMEOUT=2000
 bash
--- misc/wamp/start_httpd.sh
+++ misc/wamp/start_httpd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+export PATH=$PATH:$PWD/httpd/bin:$PWD/php
 echo "httpd is now running [ctrl+c to exit]"
 
 cd httpd
