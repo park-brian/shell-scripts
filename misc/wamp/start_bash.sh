a057f8897ef718e3ba1bdd0d3bf198e1a96c9db8
Brian Park <re.public@outlook.com>
2017-06-04 09:07:46 -0400

    updated php env

--- misc/wamp/start_bash.sh
+++ misc/wamp/start_bash.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-export PATH=$PATH:$PWD/php
+export PATH=$PATH:$PWD/php:$PWD/mysql/bin:$PWD/bin
 export COMPOSER_PROCESS_TIMEOUT=2000
+# composer global require hirak/prestissimo
+
 bash
